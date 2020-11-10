57a456bc9a6db51079fcc1f489890eb16aac1519
zhang <1091231866@qq.com>
2020-11-10 20:06:08 +0800

    测试1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -81,7 +81,9 @@ public function wxEvent()
                             <MsgType><![CDATA[%s]]></MsgType>
                             <Content><![CDATA[%s]]></Content>
                         </xml>";
-
+file_put_contents ('1.txt',print_r(sprintf($temlate,$toUserName,$fromUserName,$time,$msgType,$content),1));
+file_put_contents ('2.txt',sprintf($temlate,$toUserName,$fromUserName,$time,$msgType,$content));
+die;
         echo sprintf($temlate,$toUserName,$fromUserName,$time,$msgType,$content);
     }
 
